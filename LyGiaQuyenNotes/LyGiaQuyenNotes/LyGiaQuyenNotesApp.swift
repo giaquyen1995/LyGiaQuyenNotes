153974ded1e1e21bb5ac83879096ceeb98c6626d
vfa-quyenlg <giaquyen1995@gmail.com>
2023-06-21 17:27:55 +0700

    create project

---
+++ LyGiaQuyenNotes/LyGiaQuyenNotes/LyGiaQuyenNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyGiaQuyenNotesApp.swift
+//  LyGiaQuyenNotes
+//
+//  Created by vfa on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct LyGiaQuyenNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
