31495ba024169c19495c568bb65199c841b11699
giaquyen1995 <giaquyen1995@gmail.com>
2023-06-24 20:29:54 +0700

    update unit test

--- LyGiaQuyenNotes/LyGiaQuyenNotesTests/LyGiaQuyenNotesTests.swift
+++ LyGiaQuyenNotes/LyGiaQuyenNotesTests/LyGiaQuyenNotesTests.swift
@@ -6,30 +6,123 @@
 //
 
 import XCTest
+@testable import LyGiaQuyenNotes
+import Firebase
+
 
 final class LyGiaQuyenNotesTests: XCTestCase {
+    private var result: [Note]?
+    private var error: Error?
+    private var auth:AuthDataResult?
+    var user: User?
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    override func setUp() {
+        super.setUp()
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
         }
     }
-
+    
+    func testGetMyNotes() {
+        let expectation = XCTestExpectation(description: "Completion my notes")
+        
+        Task {
+            do {
+                guard let id = Auth.auth().currentUser?.uid else {return}
+                let notes = try await API.getMyNotes(forUser: id)
+                DispatchQueue.main.async { [weak self] in
+                    self?.result = notes
+                    expectation.fulfill()
+                }
+            } catch let error {
+                DispatchQueue.main.async { [weak self] in
+                    self?.error = error
+                    expectation.fulfill()
+                }
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
+        XCTAssertNotNil(result, "Result should not be nil")
+    }
+    
+    func testGetOthersNotes() {
+        let expectation = XCTestExpectation(description: "Completion other note")
+        
+        Task {
+            do {
+                let notes = try await API.getOthersNotes()
+                DispatchQueue.main.async { [weak self] in
+                    self?.result = notes
+                    expectation.fulfill()
+                }
+            } catch let error {
+                DispatchQueue.main.async { [weak self] in
+                    self?.error = error
+                    expectation.fulfill()
+                }
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
+        XCTAssertNotNil(result, "Result should not be nil")
+    }
+    
+    
+    
+    func testFirebaseOperations() {
+        let email = "testuser\(Int.random(in: 1000...9999))@example.com"
+        let password = "TestPassword123"
+        
+        let expectation = XCTestExpectation(description: "User registration")
+        Task {
+            do {
+                let user = try await API.registerUser(email: email, password: password)
+                XCTAssertNotNil(auth, "Auth should not be nil")
+                expectation.fulfill()
+            } catch {
+                XCTFail("Sign In failed with error: \(error)")
+                expectation.fulfill()
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
+   
+    
+    func testSignIn() {
+        let email = "giquyen@gmail.com"
+        let password = "123456"
+        
+        let expectation = XCTestExpectation(description: "Sign In")
+        
+        Task {
+            do {
+                let auth = try await API.signIn(email: email, password: password)
+                XCTAssertNotNil(auth, "Auth should not be nil")
+                expectation.fulfill()
+            } catch {
+                XCTFail("Sign In failed with error: \(error)")
+                expectation.fulfill()
+            }
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    
+    
+    func testSignout() {
+        let expectation = XCTestExpectation(description: "Completion sign out")
+        Task {
+            do {
+                try Auth.auth().signOut()
+                    expectation.fulfill()
+                
+            } catch {
+                    expectation.fulfill()
+                XCTFail("Sign out failed with error")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
+    }
 }
